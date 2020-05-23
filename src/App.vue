80d2541fe48f44a5874f5beaf041689bc3ad83f2
yumauma20 <71yuma10@gmail.com>
2020-05-23 23:08:32 +0900

    styleの書き方をscssに変更

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link class="flont" to="/">Home</router-link> |
+      <router-link class="back" to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
+<style lang="scss">
 body {
   margin: 5%;
   background-color: goldenrod;
@@ -27,14 +27,12 @@ body {
 
 #nav {
   padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
