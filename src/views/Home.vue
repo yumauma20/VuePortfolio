47783affd754a48ea59f9cd4767415a5e59ea714
yumauma20 <71yuma10@gmail.com>
2020-05-25 00:02:40 +0900

    homeページの作成

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <h1>Yuma Naito</h1>
+    <h1>PortfolioSite</h1>
+    <div class="social-icon" v-for="icon in icons" :key="icon.name">
+      <a :href="icon.link">
+        {{ icon.name }}
+        {{ icon.picture }}
+      </a>
+    </div>
+</template>
+
+<script>
+  var icons = [
+    {
+      name: 'twitter',
+      link: 'https://twitter.com/yumauma20',
+      picture: 'sample.png'
+    },
+    {
+      name: 'github',
+      link: 'https://github.com/yumauma20',
+      picture: 'sample.png2'
+    }
+  ]
+</script>
+
+<style>
+
+</style>
