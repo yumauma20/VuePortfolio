d9644810bdf9ebbf85c229ed8d8e6121f9865688
yumauma20 <71yuma10@gmail.com>
2020-05-27 00:49:47 +0900

    ヘッダー部分の修正

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/">Home</router-link>|
       <router-link to="/profile">Profile</router-link> |
       <router-link to="/skill">Skill</router-link>
     </div>
