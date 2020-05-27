4aa2de7a46fab84cb6b75faa557c1d8599b9bcfa
yumauma20 <71yuma10@gmail.com>
2020-05-27 15:15:21 +0900

    snsアイコン追加

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,36 +2,56 @@
   <div id="home">
     <h1>Yuma Naito</h1>
     <h1>PortfolioSite</h1>
-    <!-- <div class="social-icon" v-for="icon in icons" :key="icon.name">
-      <a :href="icon.link">
-        {{ icon.name }}
-        {{ icon.picture }}
+    <div class="social">
+      <a href="https://twitter.com/yumauma20" class="link twitter" target="_brank">
+        <span class="fa fa-twitter"></span>
       </a>
-    </div> -->
+      <a href="https://github.com/yumauma20" class="link github" target="_brank">
+        <span class="fab fa-github"></span>
+      </a>
+    </div>
   </div>
 </template>
 
-<script>
-// var icons = [
-//   {
-//     name: 'twitter',
-//     link: 'https://twitter.com/yumauma20',
-//     picture: 'sample.png'
-//   },
-//   {
-//     name: 'github',
-//     link: 'https://github.com/yumauma20',
-//     picture: 'sample.png2'
-//   }
-// ]
-// const myApp = new Vue({
-//   data: {
-//     icons: icons
-//   }
-// })
-// myApp.$mount('home')
-</script>
-
 <style>
+  .social {
+  padding-top: 5em;
+  width: 100%;
+  top: 50%;
+  text-align: center;
+}
+.social .link {
+  display: inline-block;
+  vertical-align: middle;
+  position: relative;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  border: 2px solid black;
+  background-clip: content-box;
+  padding: 10px;
+  transition: .5s;
+  color: #D7D0BE;
+  margin: 30px;
+  font-size: 70px;
+}
+.social .link span {
+  display: block;
+  position: absolute;
+  text-align: center;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.social .link:hover {
+  padding: 20px;
+  margin-left: -5px;
+}
+.social .link.github {
+  color: black;
+}
+.social .link.twitter {
+  color: rgb(0, 183, 255);
+}
 
 </style>
